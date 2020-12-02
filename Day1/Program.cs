4971cd3d7617bba829625b6d8e9ad8b00e33edde
Beat Walti <bwalti@users.noreply.github.com>
2020-12-02 09:44:34 +0100

    day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var inputContent = await File.ReadAllTextAsync("Input.txt");
+            
+            var parts = inputContent.Split(Environment.NewLine);
+            var integers = parts.Select(int.Parse).OrderBy(i => i).ToList();
+
+            Gold1(integers);
+            Gold2(integers);
+        }
+
+        private static void Gold2(IReadOnlyList<int> integers)
+        {
+            for (var a = 0; a < integers.Count - 3; a++)
+            for (var b = a + 1; b < integers.Count - 2; b++)
+            for (var c = b + 1; c < integers.Count - 1; c++)
+            {
+                int valueA = integers[a], valueB = integers[b], valueC = integers[c];
+
+                if (valueA + valueB + valueC == 2020)
+                {
+                    Console.WriteLine(
+                        $"A: {a}, B: {b}, valueA: {valueA}, valueB: {valueB}, valueC: {valueC} -> {valueA * valueB * valueC}");
+                    return;
+                }
+            }
+        }
+
+        private static void Gold1(IReadOnlyList<int> integers)
+        {
+            for (var a = 0; a < integers.Count - 2; a++)
+            for (var b = a + 1; b < integers.Count - 1; b++)
+            {
+                int valueA = integers[a], valueB = integers[b];
+
+                if (valueA + valueB == 2020)
+                {
+                    Console.WriteLine($"A: {a}, B: {b}, valueA: {valueA}, valueB: {valueB} -> {valueA * valueB}");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public static class Program
+    {
+        private static readonly Regex PolicyMatcher = new(@"^(?<min>\d+)-(?<max>\d+) (?<char>\w): (?<password>\w+)$",
+            RegexOptions.Compiled);
+
+        public static async Task Main(string[] args)
+        {
+            var inputContent = await File.ReadAllTextAsync("Input.txt");
+            var lines = inputContent.Split(Environment.NewLine);
+
+            Gold1(lines);
+            Gold2(lines);
+        }
+
+        private static void Gold2(string[] lines)
+        {
+            var validPasswords = 0;
+            var invalidPasswords = 0;
+
+            foreach (var line in lines)
+            {
+                var matched = PolicyMatcher.Match(line);
+                if (!matched.Success) throw new InvalidOperationException("Something went really wrong :)");
+
+                var min = int.Parse(matched.Groups["min"].Value);
+                var max = int.Parse(matched.Groups["max"].Value);
+                var character = matched.Groups["char"].Value[0];
+                var password = matched.Groups["password"].Value;
+
+                var locationA = password[min - 1] == character;
+                var locationB = password[max - 1] == character;
+
+                switch (locationA, locationB)
+                {
+                    case (true, true):
+                    case (false, false):
+                        invalidPasswords++;
+                        break;
+                    
+                    case (true, false):
+                    case (false, true):
+                        validPasswords++;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Something went seriously wrong :)");
+                }
+            }
+
+            Console.WriteLine($"Valid Passwords: {validPasswords}, InvalidPasswords: {invalidPasswords}");
+        }
+
+        private static void Gold1(string[] lines)
+        {
+            var validPasswords = 0;
+            var invalidPasswords = 0;
+
+            foreach (var line in lines)
+            {
+                var matched = PolicyMatcher.Match(line);
+                if (!matched.Success) throw new InvalidOperationException("Something went really wrong :)");
+
+                var min = int.Parse(matched.Groups["min"].Value);
+                var max = int.Parse(matched.Groups["max"].Value);
+                var character = matched.Groups["char"].Value[0];
+                var password = matched.Groups["password"].Value;
+
+                var count = password.Count(c => c == character);
+                if (min <= count && count <= max)
+                    validPasswords++;
+                else
+                    invalidPasswords++;
+            }
+
+            Console.WriteLine($"Valid Passwords: {validPasswords}, InvalidPasswords: {invalidPasswords}");
+        }
+    }
+}
